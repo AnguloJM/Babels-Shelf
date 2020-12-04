96819ce680dd19b8bae1244c16d8bac0edeaf473
jairo <jairo.angulo93@gmail.com>
2020-12-04 14:57:49 -0500

    json file changed to ignore errors for deployment

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3000';
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://babels-shelf.herokuapp.com/' : 'http://localhost:3000' 
 
 const api = axios.create({
   baseURL: baseUrl
 })
 
 export default api;
+
