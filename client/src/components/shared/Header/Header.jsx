959f19e47007e6a64d46e2190017d9c45f5815ad
jairo <jairo.angulo93@gmail.com>
2020-12-06 10:35:36 -0500

    header for user that are not loged in has been restyled after being broken

--- client/src/components/shared/Header/Header.jsx
+++ client/src/components/shared/Header/Header.jsx
@@ -5,45 +5,45 @@ import { Link } from 'react-router-dom';
 const Header = (props) => {
   const { currentUser, handleLogout, currentPath } = props;
 
-  const color = currentUser ? "header-login" : "header"
+  const color = currentUser ? "header-login" && "logo-lgin" : "header" && "logo-login"
         
   return (
     <div className={color}>
-      {/* <h1 id="logo"><Link to="/" style={{ textDecoration: 'none', color:" #53352d" }}>Babel's Shelf</Link></h1> */}
       {
         currentUser ?
-          <div className="user-buttons">
-            <h1 id="logo"><Link to="/" style={{ textDecoration: 'none', color:" #F4F2E9" }}>Babel's Shelf</Link></h1>
-            <button id="logout-button" onClick={handleLogout}>Logout</button>
-            <Link to="/dashboard"><button id="return-button">Return to Dashboard</button></Link>
+          <div className ="header-login">
+            <h1 id="logo-login"><Link to="/" style={{ textDecoration: 'none', color: " #F4F2E9" }}>Babel's Shelf</Link></h1>
+            <div className="user-buttons">
+              <button id="logout-button" onClick={handleLogout}>Logout</button>
+              <Link to="/dashboard"><button id="return-button">Return to Dashboard</button></Link>
+            </div>
           </div>
           :
 
           currentPath === "" ?
-            
-            <div className="home-login-register">
+            <div className="header">
               <h1 id="logo"><Link to="/" style={{ textDecoration: 'none', color:" #53352d" }}>Babel's Shelf</Link></h1>
-              <Link to="/login"><button id="login">Login</button></Link>
-              <Link to="/register"><button id="register">Register</button></Link>
-          </div>
+              <div className="home-login-register">
+                <Link to="/login"><button id="login-button">Login</button></Link>
+                <Link to="/register"><button id="register-button">Register</button></Link>
+              </div>
+            </div>
             :
 
             currentPath === "login" ?
-              <>
-                      <h1 id="logo"><Link to="/" style={{ textDecoration: 'none', color:" #53352d" }}>Babel's Shelf</Link></h1>
-
-              <Link to="/register"><button id="register">Register</button></Link>
-            </>
+              <div className="header">
+                <h1 id="logo"><Link to="/" style={{ textDecoration: 'none', color:" #53352d" }}>Babel's Shelf</Link></h1>
+                <Link to="/register"><button id="solo-register-button">Register</button></Link>
+              </div>
             :
             currentPath === "register" ?
-                <>
-                        <h1 id="logo"><Link to="/" style={{ textDecoration: 'none', color:" #53352d" }}>Babel's Shelf</Link></h1>
-
-            <Link to="/login"><button id="login">Login</button></Link>
-            </>
+              <div className ="header">
+                <h1 id="logo"><Link to="/" style={{ textDecoration: 'none', color:" #53352d" }}>Babel's Shelf</Link></h1>
+                <Link to="/login"><button id="solo-login-button">Login</button></Link>
+              </div>
             :
-            <>
-            </>
+              <>
+              </>
       }
   
       {
